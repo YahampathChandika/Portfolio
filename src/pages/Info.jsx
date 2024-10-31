5f340f16f0cc5ba7c8ba68c06d8e63774d485172
Yahampath Chandika <yhmpth@gmail.com>
2024-11-01 00:41:37 +0530

    test

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Skills from "../components/info/Skills";
-import About from "../components/info/About";
 
 export default function Info() {
   return (
     <div>
-      <About />
       <Skills />
     </div>
   );
