a42ec1c861c5d1d3ba1457091554e4d75692900c
Yahampath Chandika <yhmpth@gmail.com>
2024-11-01 00:44:26 +0530

    abt

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Skills from "../components/info/Skills";
+import About from "../components/info/About";
 
 export default function Info() {
   return (
     <div>
+      <About />
       <Skills />
     </div>
   );
