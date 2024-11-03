e613e29bda1ff372dbc2425131180d048fef2cc2
Yahampath Chandika <yhmpth@gmail.com>
2024-11-04 00:52:30 +0530

    test

--- src/components/work/Testimonials.jsx
+++ src/components/work/Testimonials.jsx
@@ -20,7 +20,7 @@ const testimonialsData = [
       "https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/50dab922-5d48-4c6b-8725-7fd0755d9334/3a3f2d35-8167-4708-9ef0-bdaa980989f9.png",
     quote: "Their work exceeded expectations and was delivered on time.",
     contact: "987-654-3210",
-    email: "jane.smith@example.com",
+    email: "jane.sth@example.com",
   },
   // Add more testimonials as needed
 ];
